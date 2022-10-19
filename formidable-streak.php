15c36d64632c6f3c88d599979f83ab0d855172df
henrisusanto <henri@member.id>
2022-10-19 21:13:46 +0700

    kari ngetes

--- formidable-streak.php
+++ formidable-streak.php
@@ -42,67 +42,90 @@ add_shortcode('formidable-streak-achieved-100-999', function () {
     return isset($user_meta[0]) ? $user_meta[0] : 'Not achieved yet';
 });
 
-add_action('frm_after_create_entry', function ($entry_id, $form_id) {
-    $date_field_id = 999;
+add_action('frm_after_create_entry', 'streak_submission_999', 30, 2);
+add_action('frm_before_destroy_entry', 'streak_deletion_999', 10, 2);
 
+function streak_submission_999($entry_id)
+{
+    $user_id = detect_date_field_999($entry_id);
+    if (!$user_id) return true;
+
+    $entries = get_all_entries_999($user_id);
+    calculate_streak_999($user_id, $entries);
+}
+
+function streak_deletion_999($entry_id)
+{
+    $user_id = detect_date_field_999($entry_id);
+    if (!$user_id) return true;
+
+    $entries = get_all_entries_999($user_id);
+    $entries = array_filter($entries, function ($entry) use ($entry_id) {
+        return $entry->entry_id != $entry_id;
+    });
+    calculate_streak_999($user_id, $entries);
+}
+
+function detect_date_field_999($entry_id)
+{
     global $wpdb;
-    $prefix = $wpdb->prefix;
+    $detector = $wpdb->prepare("
+        SELECT
+            entry.user_id
+        FROM {$wpdb->prefix}frm_items entry
+        LEFT JOIN {$wpdb->prefix}frm_forms form ON entry.form_id = form.id
+        LEFT JOIN {$wpdb->prefix}frm_fields field ON field.form_id = form.id
+        WHERE TRUE
+        AND entry.id = %d
+        AND field.id = %d
+    ", $entry_id, 999);
+    $detected = $wpdb->get_row($detector);
+    return !is_null($detected) ? $detected->user_id : false;
+}
 
-    // DETECT IF ANY STREAK SUBMITTED
-    $any_streak_submitted_query = $wpdb->prepare("
+function get_all_entries_999($user_id)
+{
+    global $wpdb;
+    $entries = $wpdb->prepare("
         SELECT
             entry.id entry_id
-            , entry.user_id
-            , answer.id answer_id
             , answer.meta_value answer_value
-        FROM {$prefix}frm_items entry
-        LEFT JOIN {$prefix}frm_item_metas answer ON answer.item_id = entry.id
+        FROM {$wpdb->prefix}frm_items entry
+        RIGHT JOIN {$wpdb->prefix}frm_item_metas answer ON answer.item_id = entry.id
         WHERE TRUE
-            AND entry.id = %d
-            AND answer.field_id = %d
-    ", $entry_id, $date_field_id);
-    $submitted_streak = $wpdb->get_row($any_streak_submitted_query);
-    if (is_null($submitted_streak)) return true;
+        AND entry.user_id = %d
+        AND answer.field_id = %d
+    ", $user_id, 999);
+    return $wpdb->get_results($entries);
+}
 
-    // CALCULATE LAST STREAK
-    $last_streak = 0;
-    $collect_date_query = $wpdb->prepare("
-        SELECT
-            answer.meta_value streak_date
-        FROM {$wpdb->prefix}frm_item_metas answer
-        LEFT JOIN {$wpdb->prefix}frm_items entry ON entry.id = answer.item_id
-        WHERE TRUE
-            AND answer.field_id = %d
-            AND entry.user_id = %d
-        ORDER BY DATE(answer.meta_value) DESC
-    ", $date_field_id, $submitted_streak->user_id);
-    $dates = $wpdb->get_results($collect_date_query);
-
+function calculate_streak_999($user_id, $entries)
+{
+    $streak = 0;
     $dates = array_map(function ($date) {
-        return $date->streak_date;
-    }, $dates);
+        return $date->answer_value;
+    }, $entries);
 
     $day = new DateTime();
-    $day->modify('-1 day');
     while (in_array($day->format('Y-m-d'), $dates)) {
-        $last_streak++;
+        $streak++;
         $day->modify('-1 day');
     }
 
-    // UPDATE LAST STREAK
-    update_user_meta((int)$submitted_streak->user_id, "ENTRY_USER_META_LAST_STREAK_999", $last_streak);
+    // UPDATE STREAK
+    update_user_meta((int)$user_id, "ENTRY_USER_META_STREAK_999", $streak);
 
     // UPDATE BEST STREAK
-    $best_streak = get_user_meta((int)$submitted_streak->user_id, "ENTRY_USER_META_BEST_STREAK_999");
+    $best_streak = get_user_meta((int)$user_id, "ENTRY_USER_META_BEST_STREAK_999");
     $best_streak = isset($best_streak[0]) ? $best_streak[0] : 0;
-    if ((int) $last_streak >= (int) $best_streak) update_user_meta((int)$submitted_streak->user_id, "ENTRY_USER_META_BEST_STREAK_999", $last_streak);
+    if ((int) $streak >= (int) $best_streak) update_user_meta((int)$user_id, "ENTRY_USER_META_BEST_STREAK_999", $streak);
 
     // UPDATE ACHIEVED-10
-    if (10 === (int)$last_streak) update_user_meta((int)$submitted_streak->user_id, "ENTRY_USER_META_STREAK_10_999", 'Achieved');
+    if (10 === (int)$streak) update_user_meta((int)$user_id, "ENTRY_USER_META_STREAK_10_999", 'Achieved');
 
     // UPDATE ACHIEVED-100
-    if (100 === (int)$last_streak) update_user_meta((int)$submitted_streak->user_id, "ENTRY_USER_META_STREAK_100_999", 'Achieved');
+    if (100 === (int)$streak) update_user_meta((int)$user_id, "ENTRY_USER_META_STREAK_100_999", 'Achieved');
 
     // UPDATE FIRST DAY OF LAST STREAK
-    update_user_meta((int)$submitted_streak->user_id, "ENTRY_USER_META_FIRST_DAY_LAST_STREAK_999", $day->format('Y-m-d'));
-}, 30, 2);
+    update_user_meta((int)$user_id, "ENTRY_USER_META_FIRST_DAY_LAST_STREAK_999", $day->format('Y-m-d'));
+}
